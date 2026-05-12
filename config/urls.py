cacf647545016db981a3094406163ff4c2d49fea
Iván González <ivann-gonzalez@hotmail.com>
2026-05-12 10:20:09 -0300

    Ruta principal

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('asistencias.urls')),
-    
+
+    path('', views.inicio),
+
+    path('inicio/', views.inicio),
 ]
