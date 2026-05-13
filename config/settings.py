6df78943066139d7c416e62b55dece8a17021d5d
Iván González <ivann-gonzalez@hotmail.com>
2026-05-13 10:11:17 -0300

    Corregir messages en Render

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
